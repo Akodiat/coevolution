831e7aa87718595d98e30be0238d8c19972a9f4e
Akodiat <joakimjo@swipnet.se>
2015-11-30 10:16:30 +0100

    Removed unused imports

--- eclipse/Coevolution/src/tiles/Anglerfish.java
+++ eclipse/Coevolution/src/tiles/Anglerfish.java
@@ -1,8 +1,5 @@
 package tiles;
 
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
--- eclipse/Coevolution/src/tiles/Prey.java
+++ eclipse/Coevolution/src/tiles/Prey.java
@@ -1,8 +1,6 @@
 package tiles;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
